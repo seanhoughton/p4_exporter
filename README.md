d4bc1c6f193994240535cdc7a212de9f87068443
Sean Houghton <sean.houghton@activision.com>
2017-01-12 15:48:56 -0800

    Pass credentials on the command line

--- README.md
+++ README.md
@@ -2,7 +2,14 @@
 
 Exports basic information about a remote Perforce server on port 9666. 
 
-    http://localhost:9666/metrics?port=perforce:1666&username=myuser&password=mypass
+    http://localhost:9666/metrics?port=perforce:1666&username=myuser
+
+
+## Authentication/Credentials
+
+Credentials for servers can be passed in on the command line or as the `P4EXP_CREDENTIALS` environment variable. A credential must exist for all queried servers.
+
+    p4exporter.py -c foo:bar@perforce1,jar:car@perforce2
 
 
 ## Query Parameters
@@ -12,7 +19,6 @@ Exports basic information about a remote Perforce server on port 9666.
 | --------- | --------------------------------------------------- |
 | port      | address and port of the P4 server, i.e. server:1666 |
 | username  | username                                            |
-| password  | password or token                                   |
 
 
 ## Target configuration
--- p4exporter.py
+++ p4exporter.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import os
+import re
 from argparse import ArgumentParser
 from prometheus_client import start_http_server
 from prometheus_client.core import GaugeMetricFamily, CounterMetricFamily, REGISTRY
@@ -8,10 +10,18 @@ import logging
 from P4 import P4
 
 
+CREDENTIAL_REGEX = r'(?P<username>.+)\:(?P<password>.+)\@(?P<hostname>.+)'
+
+
 class P4Collector(object):
 
-    def __init__(self):
-        self.__cache = {}
+    def __init__(self, credentials):
+        parsed_credentials = [re.match(CREDENTIAL_REGEX, c).groupdict() for c in credentials.split(',')]
+        self.credentials = {self.credential_key(c['hostname'], c['username']): c['password'] for c in parsed_credentials}
+        logging.info("Loaded %d credential", len(self.credentials))
+
+    def credential_key(self, hostname, username):
+        return '%s-%s' % (username, hostname)
 
     def name(self, name):
         return 'p4_' + name
@@ -54,9 +64,15 @@ class P4Collector(object):
         if not params:
             return
         p4 = P4()
+        hostname, port = params['port'][0].split(':')
+        username = params['username'][0]
+        p4.user = username
         p4.port = params['port'][0]
-        p4.user = params['username'][0]
-        p4.password = params['password'][0]
+        credential_key = self.credential_key(hostname, username)
+        if credential_key not in self.credentials:
+            logging.error('No credentials for %s@%s', username, hostname)
+            return
+        p4.password = self.credentials[credential_key]
         try:
             start_time = time.time()
             logging.debug('Connecting to %s@%s...', params['username'][0], params['port'][0])
@@ -74,20 +90,21 @@ class P4Collector(object):
         yield self.workspaces(p4)
         yield self.users(p4)
         yield self.changelist(p4)
-        depot_sizes, depot_counts, created_guage = self.depot_guages(p4)
-        yield depot_sizes
-        yield depot_counts
-        yield created_guage
+        #depot_sizes, depot_counts, created_guage = self.depot_guages(p4)
+        #yield depot_sizes
+        #yield depot_counts
+        #yield created_guage
 
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true', default=False, help='Enable verbose logging')
     parser.add_argument('-p', '--port', dest='port', type=int, default=9666, help='The port to expose metrics on, default: 9666')
+    parser.add_argument('-c', '--credentials', dest='credentials', default=os.environ.get('P4EXP_CREDENTIALS', ''), help='A command delimited set of credentials in <username>:<password>@<hostname> format.')
     options = parser.parse_args()
     logging.basicConfig(level=logging.DEBUG if options.verbose else logging.INFO, format='[%(levelname)s] %(message)s')
     logging.info('Creating collector...')
-    REGISTRY.register(P4Collector())
+    REGISTRY.register(P4Collector(options.credentials))
     logging.info('Listening on port :%d...', options.port)
     start_http_server(options.port)
     while True:
